97b1b4ce8de57e000354fd847ae3e3b4a5a8009b
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-05-24 11:25:33 +0000

    STYLE:customizing how models are displayed

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    ordering = ["status", "publish"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    show_facets = admin.ShowFacets.ALWAYS
