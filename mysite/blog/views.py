40a4be095d5df91765b90605307e494f08cabe14
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-05-28 23:01:28 +0000

    FEAT: add pagination and pagination template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from .models import Post
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 1)
+    page_number = request.GET.get("page", 1)
+    posts = paginator.page(page_number)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
