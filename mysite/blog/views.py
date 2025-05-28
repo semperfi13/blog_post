346e1e9d45e738f7c6d6ff72d0314ca1f6d639cd
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-05-28 23:32:12 +0000

    FEAT: handking pagination error

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,13 +3,19 @@ from .models import Post
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 1)
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
