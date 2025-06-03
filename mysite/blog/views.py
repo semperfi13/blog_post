49c392dfb6a9d45f213895ffda901f44f917eac1
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-06-03 10:06:30 +0000

    FEAT: Handling forms in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -41,3 +42,15 @@ def post_detail(request, year, month, day, post):
     )
 
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+        else:
+            form = EmailPostForm()
+
+    return render(request, "blog/post/share.html", {"post": post, "form": form})
