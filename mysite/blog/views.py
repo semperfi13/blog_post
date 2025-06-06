b36128d6fb106e84113972ac704a0864db81e521
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-06-06 14:39:11 +0000

    FEAT: sending emails via views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -47,11 +47,35 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']})" f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} as {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}'"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
         else:
             form = EmailPostForm()
 
-    return render(request, "blog/post/share.html", {"post": post, "form": form})
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form,
+            "sent": sent,
+        },
+    )
