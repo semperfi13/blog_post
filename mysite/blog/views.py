2aa344bfe3fea200146bb62bc6cc0a20fb098684
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-06-11 23:17:56 +0000

    FEAT: adding comment to the post details view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -42,8 +42,16 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for the users to comment
+    form = CommentForm()
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
 
 
 def post_share(request, post_id):
