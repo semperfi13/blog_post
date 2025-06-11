e9c92eef3dc86c89c4972aa8c7de7a88b0db2b78
Adamou Nikiema <adamsnikiema187@gmail.com>
2025-06-11 23:01:48 +0000

    FEAT: handling modelforms in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 class PostListView(ListView):
@@ -78,4 +79,20 @@ def post_share(request, post_id):
             "form": form,
             "sent": sent,
         },
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    commnent = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
     )
